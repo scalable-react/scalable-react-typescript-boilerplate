62a1e1bd1d2c07dbb08658bd410cf9c5165a0f4a
Ryan C. Collins <admin@ryancollins.io>
2017-01-05 17:32:02 -0500

    Feat: update typescript description text for features section

--- src/containers/Features/features.ts
+++ src/containers/Features/features.ts
@@ -10,5 +10,9 @@ export default {
   Code reuse has _never_ been easier.  Organizing your UI components by feature, rather than file-type, will help you to reuse code and facilitates building of UI component libraries.
 
   We've included component / container generators for you to easily get started using the feature-first architecture.  Try it __now__ and thank us later!`,
-  typeScript: `TypeScript is a _typed superset_ of JavaScript that compiles to plain JavaScript!`,
+  typeScript: `TypeScript is a _typed superset_ of JavaScript that compiles to plain JavaScript!
+
+  TypeScript allows you to write the JavaScript you are used to while adding type annotiations that provide you with powerful static analysis.
+
+  It helps to [bridge the gap](https://medium.com/@ryancollinsio/bridging-the-gap-between-elm-and-javascript-with-typescript-375771ebcd4c#.64jw0aqki) between JavaScript and a fully functional language, such as Elm.`,
 };
