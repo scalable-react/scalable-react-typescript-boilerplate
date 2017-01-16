d6c4f000dbd9c3b4b531adf30a7b5b791faaf144
Ryan Collins <rccproductions@mac.com>
2017-01-15 19:01:38 -0500

    Feat: update about text

--- src/containers/About/about.ts
+++ src/containers/About/about.ts
@@ -2,11 +2,11 @@ export default {
   aboutContent: `
 # Scalable React TypeScript Boilerplate
 
-The boilerplate aims to follow best practices for building highly scalable and reusable apps and component libraries with React and cutting edge JavaScript.
+The boilerplate aims to follow best practices for building highly scalable and reusable apps and component libraries with React and cutting edge TypeScript.
+
+This project was inspired by the Scalable React Boilerplate project and inherits the same organizational strategy and general best-practices.
 
 You can read more about the organizational strategy used in this app in [this Medium post](https://medium.com/front-end-hacking/the-secret-to-organization-in-functional-programming-913484e85fc9#.6htl4s54y).
-
-We incorporate an ESLint configuration and follow strictly the [AirBnb JS & JSX style guides](https://github.com/airbnb/javascript).
 `,
 
 aboutRyan: `Hi, I'm Ryan. I’m a software engineer working on building the web one component at a time.  I am a UI / UX Engineer on the Grommet UX team, building the world's most advanced UX framework. I'm also an avid functional programmer and a student of Deep Learning / AI.`,
