03d89e45d9c604035cffe7af27079bf1048312df
Ryan Collins <rccproductions@mac.com>
2017-02-23 01:10:47 -0500

    Fix: add padding around bottom button row

--- src/client/containers/Home/index.tsx
+++ src/client/containers/Home/index.tsx
@@ -32,7 +32,7 @@ class Home extends React.Component<any, any> {
           </Hero>
         </Section>
         <Features />
-        <Section alignItems="center" backgroundColor="#f3f3f3">
+        <Section alignItems="center" pad="medium" backgroundColor="#f3f3f3">
           <GetStartedButtonRow />
         </Section>
       </Container>
