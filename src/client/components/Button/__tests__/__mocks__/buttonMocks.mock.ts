30a0c6da61fb7d37781a5d4eac5fad741e3d3bbf
Scott Fleck <scott.fleck@gmail.com>
2017-02-23 22:30:48 -0500

    Added tests for the Button component

---
+++ src/client/components/Button/__tests__/__mocks__/buttonMocks.mock.ts
@@ -0,0 +1,20 @@
+export const heroButtonTrue = {
+  isHero: true,
+};
+
+export const buttonProps = {
+  color: 'blue',
+  backgroundColor: '#ffffff',
+  borderColor: '#000000',
+  size: 'large',
+  style: 'text-align: center;',
+};
+
+export const heroButtonProps = {
+  isHero: true,
+  color: 'blue',
+  backgroundColor: '#ffffff',
+  borderColor: '#000000',
+  size: 'large',
+  style: 'text-align: center;',
+};
