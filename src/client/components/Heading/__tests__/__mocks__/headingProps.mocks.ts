e6ce98a8f3862dc3aaf83def7a818280b47fb31b
Ryan Collins <rccproductions@mac.com>
2017-02-24 21:15:55 -0500

    Feat: add multiple tests

---
+++ src/client/components/Heading/__tests__/__mocks__/headingProps.mocks.ts
@@ -0,0 +1,38 @@
+import { Margin, HeadingProps } from '../../types';
+const margin: Margin = 'none';
+
+export const h2Props: HeadingProps = {
+  tag: 'h2',
+  upcase: false,
+  truncate: true,
+  textAlign: 'left',
+  color: '#333',
+  margin,
+};
+
+export const h3Props: HeadingProps = {
+  tag: 'h3',
+  upcase: false,
+  truncate: true,
+  textAlign: 'left',
+  color: '#333',
+  margin,
+};
+
+export const h4Props: HeadingProps = {
+  tag: 'h4',
+  upcase: false,
+  truncate: true,
+  textAlign: 'left',
+  color: '#333',
+  margin,
+};
+
+export const h5Props: HeadingProps = {
+  tag: 'h5',
+  upcase: false,
+  truncate: true,
+  textAlign: 'left',
+  color: '#333',
+  margin,
+};
---
+++ src/client/components/Heading/__tests__/index.test.tsx
@@ -0,0 +1,48 @@
+import { shallow } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+import * as React from 'react';
+import Heading from '../';
+import { h2Props, h3Props, h4Props, h5Props } from './__mocks__/headingProps.mocks';
+
+describe('<Heading />', () => {
+  it('should render with default props', () => {
+    const wrapper = shallow(
+      <Heading>
+        Test
+      </Heading>,
+    );
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+  it('should render with h2 props', () => {
+    const wrapper = shallow(
+      <Heading {...h2Props}>
+        Test
+      </Heading>,
+    );
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+  it('should render with h3 props', () => {
+    const wrapper = shallow(
+      <Heading {...h3Props}>
+        Test
+      </Heading>,
+    );
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+  it('should render with h4 props', () => {
+    const wrapper = shallow(
+      <Heading {...h4Props}>
+        Test
+      </Heading>,
+    );
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+  it('should render with h5 props', () => {
+    const wrapper = shallow(
+      <Heading {...h5Props}>
+        Test
+      </Heading>,
+    );
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+});
