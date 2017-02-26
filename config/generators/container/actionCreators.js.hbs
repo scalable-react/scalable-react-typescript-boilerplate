02105ee810b177d9db4f9b3629bba97695584de3
Ryan Collins <rccproductions@mac.com>
2017-02-26 00:01:26 -0500

    Fix: broken mocks that were compiled

---
+++ config/generators/container/actionCreators.js.hbs
@@ -0,0 +1,35 @@
+import * as T from './constants';
+import {
+  LoadInitiationAction,
+  LoadSuccessAction,
+  LoadFailureAction,
+  LoadCancelAction,
+} from './actions';
+import { ErrorType } from './types';
+
+export const loadInitiation = (): LoadInitiationAction => ({
+  type: T.LOAD_INITIATION,
+});
+
+export const loadSuccess = (data: any): LoadSuccessAction => ({
+  type: T.LOAD_SUCCESS,
+  payload: data,
+});
+
+export const loadFailure = (error: ErrorType): LoadFailureAction => ({
+  type: T.LOAD_FAILURE,
+  payload: error,
+});
+
+export const loadCancel = (): LoadCancelAction => ({
+  type: T.LOAD_CANCEL,
+});
+
+export const actionCreators = {
+  loadInitiation,
+  loadSuccess,
+  loadFailure,
+  loadCancel,
+};
+
+export default actionCreators;
--- src/client/containers/Docs/actions.ts
+++ src/client/containers/Docs/actions.ts
@@ -1,21 +1,22 @@
+import { PayloadAction } from '../../types';
 import * as types from './constants';
 
-export interface LoadInitiationAction {
-  type: types.LOAD_INTIATION_TYPE,
+export interface LoadInitiationAction extends PayloadAction<undefined> {
+  type: types.LOAD_INTIATION_TYPE;
 }
 
-export interface LoadSuccessAction  {
-  type: types.LOAD_SUCCESS_TYPE,
-  payload: string
+export interface LoadSuccessAction extends PayloadAction<string> {
+  type: types.LOAD_SUCCESS_TYPE;
+  payload: string;
 }
 
-export interface LoadFailureAction {
-  type: types.LOAD_FAILURE_TYPE,
-  payload: string
+export interface LoadFailureAction extends PayloadAction<string> {
+  type: types.LOAD_FAILURE_TYPE;
+  payload: string;
 }
 
-export interface LoadCancelAction {
-  type: types.LOAD_CANCEL_TYPE,
+export interface LoadCancelAction extends PayloadAction<string> {
+  type: types.LOAD_CANCEL_TYPE;
 }
 
 export type DocsAction = LoadInitiationAction | LoadSuccessAction | LoadFailureAction | LoadCancelAction;
