41e4c94c3aaaa085236203a5382566d14a1f8917
Gabry <gabriele.cimato@gmail.com>
2017-02-26 14:26:14 -0800

    Test: add Comment tests

---
+++ src/client/components/Comment/__tests__/__mocks__/commentMocks.mock.ts
@@ -0,0 +1,10 @@
+export const commentProps = {
+  author: 'Comment Author',
+  body: 'Comment Body',
+};
+
+export const commentPicProps = {
+  author: 'Comment Author',
+  body: 'Comment Body',
+  picUrl: 'https://github.com/RyanCCollins/cdn/blob/master/misc/missing.png?raw=true',
+};
