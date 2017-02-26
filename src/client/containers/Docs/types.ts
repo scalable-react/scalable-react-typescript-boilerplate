84a7498f77e58e7178abfd36dc0b7689dc327f3e
Ryan Collins <RyanCCollins@users.noreply.github.com>
2017-02-25 20:43:05 -0500

    Update types.ts

--- src/client/containers/Docs/types.ts
+++ src/client/containers/Docs/types.ts
@@ -2,7 +2,7 @@
 export * from './actions';
 // export {LOAD_CANCEL_TYPE, LOAD_FAILURE_TYPE, LOAD_INTIATION_TYPE, LOAD_SUCCESS_TYPE } from './constants'; 
 export * from './index';
-export {DocsProps} from './presentation';
+export { DocsProps } from './presentation';
 
-export {State} from './state';
+export { State } from './state';
 export * from './types';
