69c695bb310e7f9350ef9a45a7745378fa3238ec
Abhishek Ghosh <abhighosh18@gmail.com>
2017-02-26 16:35:06 -0800

    Feat: Tests for Navbar component

---
+++ src/client/components/NavBar/__tests__/__mocks__/navBarMocks.mock.ts
@@ -0,0 +1,15 @@
+const links = [
+  {
+    url: 'https://medium.com',
+    text: 'Medium',
+  },
+  {
+    url: 'https://www.ryancollins.io/',
+    text: 'RyanCollins',
+  },
+];
+
+export const navbarProps = {
+  logoText: 'NavBar Test',
+  links,
+};
