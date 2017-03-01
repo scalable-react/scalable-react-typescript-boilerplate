a09aafd45aaab38596ade39aa1576d66cdffba23
Scott Fleck <scott.fleck@gmail.com>
2017-02-28 20:47:48 -0500

    Adjusted prop types on Error element. Added tests.

--- src/client/components/Error/index.tsx
+++ src/client/components/Error/index.tsx
@@ -6,12 +6,17 @@ export default function Error({
   message,
   onClose,
 }: ErrorProps): JSX.Element {
+
+  // Box extends HtmlProps so onClick will not accept a "Function" type input.
+  // Cast the onClose prop to a React.Eventhandler type to allow a function. 
+  const onCloseCallback = onClose as React.EventHandler<any>;
+
   return (
     <Box
       backgroundColor="#ff324d"
       boxSize={{ horizontal: 'medium' }}
       pad="small"
-      onClick={onClose}
+      onClick={onCloseCallback}
       alignItems="center"
     >
       <Paragraph size="large" color="white">{message}</Paragraph>
