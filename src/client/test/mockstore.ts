d2430a7a98d3903c67349539d837f8c19741291c
Steven T. Cramer <StevenTCramer@gmail.com>
2017-02-28 19:40:31 -0500

    use root as top level name for things

--- src/client/test/mockstore.ts
+++ src/client/test/mockstore.ts
@@ -2,14 +2,14 @@
 import configureMockStore from 'redux-mock-store';
 import { Middleware } from 'redux';
 import { createLogicMiddleware } from 'redux-logic';
-import appLogic from '../logic';
+import rootLogic from '../logic';
 import axios from 'axios';
 
 const dependencies = {
   httpClient: axios,
 };
 
-const logicMiddleware = createLogicMiddleware(appLogic, dependencies);
+const logicMiddleware = createLogicMiddleware(rootLogic, dependencies);
 
 const middlewares: Middleware[] = [
   logicMiddleware,
