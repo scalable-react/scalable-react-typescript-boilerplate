2ca24b701749eef3f8a2ff59f31c726899824178
Abhishek Ghosh <abhighosh18@gmail.com>
2017-03-03 14:33:36 -0800

    Feat: Add info for Button storybook

--- config/.storybook/stories/components/Button.js
+++ config/.storybook/stories/components/Button.js
@@ -4,24 +4,52 @@ import { Button } from 'components';
 import { ButtonSize } from 'components/Button';
 
 storiesOf('Button', module)
-  .add('default button', () =>
-    <Button
+  .addWithInfo(
+    'default Button',
+    `
+      This is a default button with a \`onClick\` handler. Usage: 
+      ~~~js
+        <Button onClick={this.props.handleClick} />
+      ~~~
+    `,
+     () => (
+      <Button
       onClick={action('clicked the default button')}
       backgroundColor="#c05b4d"
       borderColor="#732419"
-    >
-      Default button
-    </Button>
+      >
+        Default button
+      </Button>
+     ),
+     {
+       inline: true,
+       propTable: [Button]
+     }
   )
-  .add('hero button', () =>
-    <Button
-      onClick={action('clicked the hero button')}
-      style={{ margin: 10 }}
-      size={ButtonSize.xlarge}
-      isHero
-      backgroundColor="#c05b4d"
-      borderColor="#732419"
-    >
-      Hero Button
-    </Button>
+  .addWithInfo(
+    'hero button',
+    `
+      This is a button with \`isHero\` prop set to \`true\`. Usage: 
+      ~~~js
+        <Button 
+          isHero,
+          onClick={this.props.handleClick} 
+        />
+      ~~~
+    `,
+     () => (
+      <Button
+        onClick={action('clicked the hero button')}
+        style={{ margin: 10 }}
+        size={ButtonSize.xlarge}
+        isHero
+        backgroundColor="#c05b4d"
+        borderColor="#732419"
+      >
+        Hero Button
+      </Button>
+     ),
+     {
+       inline: true,
+     }
   );
