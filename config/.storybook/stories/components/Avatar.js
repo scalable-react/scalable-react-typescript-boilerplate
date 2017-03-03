b29a641f11839c35cc1af46077aaf8f5a1002ce5
Abhishek Ghosh <abhighosh18@gmail.com>
2017-03-03 13:54:21 -0800

    Feat: Add Avatar to storybook

--- config/.storybook/stories/components/Avatar.js
+++ config/.storybook/stories/components/Avatar.js
@@ -6,28 +6,116 @@ const contributor = {
   name: 'Ryan Collins',
   src: 'https://avatars0.githubusercontent.com/u/13810084?v=3&s=460',
 };
-storiesOf('Avatar', module)
-  .add('default avatar', () =>
-      <Avatar />
+
+storiesOf('Avatar')
+  .addWithInfo(
+    'Default Avatar',
+    `
+      This is the basic usage of \`<Avatar /> \` component. It gets default props: \`name: Contributor \`
+      and a default url for avatar image.
+      ~~~js
+        <Avatar />
+      ~~~
+    `,
+    () => (
+      <div>
+        <Avatar />
+      </div>
+    ),
+    {
+      inline: true,
+    }
   )
-  .add('thumb avatar', () =>
-    <Avatar
-      {...contributor}
-      size="thumb"
-    />
-  ).add('small avatar', () =>
-    <Avatar
-      {...contributor}
-      size="small"
-    />
-  ).add('medium avatar', () =>
-    <Avatar
-      {...contributor}
-      size="medium"
-    />
-  ).add('large avatar', () =>
-    <Avatar
-      {...contributor}
-      size="large"
-    />
+  .addWithInfo(
+    'size="thumb"',
+    `
+      \`Usage:\`
+      ~~~js
+        <Avatar 
+          size="thumb"
+          name='John Doe'
+          src='/src/to/avatar' 
+        />
+      ~~~
+    `,
+    () => (
+      <div>
+        <Avatar 
+          {...contributor}
+          size="thumb" 
+        />
+      </div>
+    ),
+    {
+      inline: true,
+    }
+  ).addWithInfo(
+    'size="small"',
+    `
+      \`Usage:\`
+      ~~~js
+        <Avatar 
+          size="small"
+          name='John Doe'
+          src='/src/to/avatar' 
+        />
+      ~~~
+    `,
+    () => (
+      <div>
+        <Avatar 
+          {...contributor}
+          size="small" 
+        />
+      </div>
+    ),
+    {
+      inline: true,
+    }
+  ).addWithInfo(
+    'size="medium"',
+    `
+      \`Usage:\`
+      ~~~js
+        <Avatar 
+          size="medium"
+          name='John Doe'
+          src='/src/to/avatar' 
+        />
+      ~~~
+    `,
+    () => (
+      <div>
+        <Avatar 
+          {...contributor}
+          size="medium" 
+        />
+      </div>
+    ),
+    {
+      inline: true,
+    }
+  ).addWithInfo(
+    'size="large"',
+    `
+      \`Usage:\`
+      ~~~js
+        <Avatar 
+          size="large"
+          name='John Doe'
+          src='/src/to/avatar' 
+        />
+      ~~~
+    `,
+    () => (
+      <div>
+        <Avatar 
+          {...contributor}
+          size="large" 
+        />
+      </div>
+    ),
+    {
+      inline: true,
+    }
   );
