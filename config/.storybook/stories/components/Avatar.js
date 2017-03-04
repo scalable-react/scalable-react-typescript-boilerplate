0f54d0ca6fba3d0b8284ecec5c451ef1a87d45f7
Abhishek Ghosh <abhighosh18@gmail.com>
2017-03-04 09:09:06 -0800

    Feat: Integrate knobs addon

--- config/.storybook/stories/components/Avatar.js
+++ config/.storybook/stories/components/Avatar.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import { Avatar } from 'components';
+import { 
+  withKnobs, 
+  text, boolean, number 
+} from '@kadira/storybook-addon-knobs';
 
 const contributor = {
   name: 'Ryan Collins',
   src: 'https://avatars0.githubusercontent.com/u/13810084?v=3&s=460',
 };
 
-storiesOf('Avatar')
+const stories = storiesOf('Avatar stories', module);
+stories.addDecorator(withKnobs);
+
+stories
   .addWithInfo(
+    'with src Avatar',
+    `
+      Some documentation!
+    `,
+     () => (
+    <Avatar
+      name={text('Name', 'Ryan Collins')}
+      src={text('URL','https://avatars0.githubusercontent.com/u/13810084?v=3&s=460')}
+    />
+    ),
+    {
+      inline: true,
+    }
+  );
+  /*.addWithInfo(
     'Default Avatar',
     `
       This is the basic usage of \`<Avatar /> \` component. It gets default props: \`name: Contributor \`
@@ -118,4 +140,4 @@ storiesOf('Avatar')
     {
       inline: true,
     }
-  );
+  );*/
