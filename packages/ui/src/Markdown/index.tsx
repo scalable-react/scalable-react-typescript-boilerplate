07df4d095bbcc16a42e698386b69ebec3681139c
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2017-03-15 16:43:36 -0400

    Load markdown component

--- packages/ui/src/Markdown/index.tsx
+++ packages/ui/src/Markdown/index.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import { MarkdownProps } from './types';
 import Component from './styles';
-import ReactMarkdown from 'react-markdown';
+
+// will need to track this down more later but require works -- import gets undefined
+var ReactMarkdown = require('react-markdown');
+
 import 'github-markdown-css/github-markdown.css';
 
 export default function Markdown({
