2baa8adebc35ab01b2fc1f7e440413d736d0e9e7
Ryan Collins <RyanCCollins@users.noreply.github.com>
2017-03-20 22:24:43 -0400

    Update actionCreators.ts

--- src/client/containers/App/actionCreators.ts
+++ src/client/containers/App/actionCreators.ts
@@ -1,5 +1,5 @@
 import * as types from './constants';
-import {defaultAction} from 'shared/actionCreators';
+import { defaultAction } from 'shared/actionCreators';
 import {
   SetIsMobileAction,
 } from './actions';
