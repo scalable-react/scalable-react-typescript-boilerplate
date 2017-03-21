221ecdade0205447701d2c79d392c3562764b8ae
Ryan Collins <rccproductions@mac.com>
2017-03-20 22:10:04 -0400

    Feat: update generators with new architecture

--- config/generators/container/index.js
+++ config/generators/container/index.js
@@ -43,26 +43,50 @@ module.exports = {
     const containerPath = path.resolve(process.cwd(), `${data.path}/{{properCase name}}/`);
     const rootPath = path.resolve(process.cwd(), `./src/client/containers/index.ts`);
     const reducersPath = path.resolve(process.cwd(), './src/client/reducers.ts');
+    const typesPath = path.resolve(process.cwd(), './src/client/types.ts');
     const statePath = path.resolve(process.cwd(), './src/client/state.ts');
-    const actions = [{
-      type: 'add',
-      path: `${containerPath}/index.tsx`,
-      templateFile: './container/index.js.hbs',
-      abortOnFail: true
-    }, {
-      type: 'modify',
-      path: rootPath,
-      pattern: /(\/\* GENERATOR-IMPORT \*\/)/g,
-      template: trimTemplateFile('./config/generators/container/import.js.hbs'),
-      abortOnFail: false
-    },
-    {
-      type: 'modify',
-      path: rootPath,
-      pattern: /(\/\* GENERATOR-EXPORT \*\/)/g,
-      template: trimTemplateFile('./config/generators/container/export.js.hbs'),
-      abortOnFail: false
-    }];
+    const actions = [
+      {
+        type: 'add',
+        path: `${containerPath}/index.tsx`,
+        templateFile: './container/index.js.hbs',
+        abortOnFail: true
+      },
+      {
+        type: 'add',
+        path: `${containerPath}/presentation.tsx`,
+        templateFile: './container/presentation.js.hbs',
+        abortOnFail: true
+      },
+      {
+        type: 'modify',
+        path: rootPath,
+        pattern: /(\/\* GENERATOR-IMPORT \*\/)/g,
+        template: trimTemplateFile('./config/generators/container/import.js.hbs'),
+        abortOnFail: false
+      },
+      {
+        type: 'modify',
+        path: rootPath,
+        pattern: /(\/\* GENERATOR-EXPORT \*\/)/g,
+        template: trimTemplateFile('./config/generators/container/export.js.hbs'),
+        abortOnFail: false
+      },
+      {
+        type: 'modify',
+        path: typesPath,
+        pattern: /(\/\* GENERATOR-IMPORT \*\/)/g,
+        template: trimTemplateFile('./config/generators/container/types.import.ts.hbs'),
+        abortOnFail: false
+      },
+      {
+        type: 'modify',
+        path: typesPath,
+        pattern: /(\/\* GENERATOR-EXPORT \*\/)/g,
+        template: trimTemplateFile('./config/generators/container/types.export.ts.hbs'),
+        abortOnFail: false
+      }
+    ];
 
     actions.push({
       type: 'add',
@@ -88,6 +112,14 @@ module.exports = {
         type: 'add',
         path: `${containerPath}/actionCreators.ts`,
         templateFile: './container/actionCreators.js.hbs',
+        abortOnFail: true
+      });
+
+
+      actions.push({
+        type: 'add',
+        path: `${containerPath}/state.ts`,
+        templateFile: './container/state.js.hbs',
         abortOnFail: true
       });
 
