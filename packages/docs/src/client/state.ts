89b9c6ce78e5b6fda59a7080ce43aeada1d378ef
Ryan Collins <rccproductions@mac.com>
2017-04-25 19:53:57 -0400

    Feat: update failing snapshot tests

--- packages/docs/src/client/state.ts
+++ packages/docs/src/client/state.ts
@@ -1,8 +1,8 @@
 /* GENERATOR-IMPORT-STATE */
-import { initialState as appState, State as AppState } from 'containers/App/state';
-import { initialState as docsState, State as DocsState } from 'containers/Docs/state';
-import { initialState as todoAppState, State as TodoAppState } from 'containers/TodoApp/state';
-import { initialState as blogPostState, State as BlogPostState } from 'containers/BlogPost/state';
+import { initialState as appState, State as AppState } from './containers/App/state';
+import { initialState as docsState, State as DocsState } from './containers/Docs/state';
+import { initialState as todoAppState, State as TodoAppState } from './containers/TodoApp/state';
+import { initialState as blogPostState, State as BlogPostState } from './containers/BlogPost/state';
 
 export interface State {
   /* GENERATOR-EXPORT-STATE-TYPE */
