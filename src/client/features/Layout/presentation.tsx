2d5599220ac6d2a10829e27190a29b89a7f73238
Ryan Collins <RyanCCollins@users.noreply.github.com>
2017-07-03 07:33:53 -0400

    Update presentation.tsx

--- src/client/features/Layout/presentation.tsx
+++ src/client/features/Layout/presentation.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Props } from './types';
 import Main from './main';
 
-export default function Presentation({ children }: Props): JSX.Element {
+export default function LayoutPresentation({ children }: Props): JSX.Element {
   return (
     <Main>
       {React.cloneElement(children)}
